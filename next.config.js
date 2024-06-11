6c1cf142e1e4d9331ad5de26d6d2645682bfafc6
Krisna Rusdiono <krisnarusdiono@gmail.com>
2024-06-11 08:41:20 +0700

    chore: configure response headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   output: "standalone",
+  poweredByHeader: false,
+  headers: () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'Cross-Origin-Opener-Policy',
+          value: 'same-origin-allow-popups',
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff',
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'origin-when-cross-origin',
+        },
+        {
+          key: 'Permissions-Policy',
+          value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()',
+        },
+      ],
+    }
+  ]
 };
